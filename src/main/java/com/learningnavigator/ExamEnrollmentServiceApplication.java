0e2f5c61fc0f2a006699e01f1aca0d7be3b72e95
Deepak Sharma <46669654+deepakprogrammer@users.noreply.github.com>
2024-05-19 09:12:31 +0530

    just workspace setup

---
+++ src/main/java/com/learningnavigator/ExamEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learningnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamEnrollmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamEnrollmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningnavigator/ExamEnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamEnrollmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
